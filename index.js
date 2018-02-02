249b764852b72b700600d06df8052b4ac47bd401
lufinkey <luisfinke@gmail.com>
2018-02-02 16:42:22 -0500

    read from /etc/passwd on linux

--- index.js
+++ index.js
@@ -23,6 +23,36 @@ function homedir(username)
 {
 	switch(os.platform())
 	{
+		case 'linux':
+			var passwd = fs.readFileSync('/etc/passwd');
+			if(passwd == null)
+			{
+				// passwd file doesn't exist for some reason
+				throw new Error("missing /etc/passwd file");
+			}
+			//convert passwd to a string
+			passwd = passwd.toString();
+			//split lines and read line by line
+			var lines = passwd.split('\n');
+			for(var line of lines)
+			{
+				var parts = line.split(':');
+				if(parts[0] == username)
+				{
+					var userPath = parts[5];
+					if(userPath === "")
+					{
+						return null;
+					}
+					else if(userPath == null)
+					{
+						throw new Error("bad /etc/passwd file format")
+					}
+					return userPath;
+				}
+			}
+			throw new Error("user does not exist");
+
 		default:
 			var userPath = username ? path.resolve(path.dirname(home), username) : home;
 			if(!fs.existsSync(userPath))
